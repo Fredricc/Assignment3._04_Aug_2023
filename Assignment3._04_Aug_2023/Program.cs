470b31a1bd19cba557363175c2201755da0d3ac4
Fredrick Njuguna <kfred45.fk@gmail.com>
2023-08-07 08:46:33 +0300

    Add project files.

---
+++ Assignment3._04_Aug_2023/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Assignment3._04_Aug_2023
+{
+    class Program
+    {
+        public static void Main()
+        {
+
+            Console.WriteLine("Enter the size of the array:");
+            int size = Convert.ToInt32(Console.ReadLine());
+            int[] arr = new int[size];
+
+            Console.WriteLine($"Enter the  {size}  members of the array.");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.In.ReadLine());
+            }
+
+            Console.WriteLine("Your Array is as follows");
+            Console.Write( "[ " );
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine("]");
+            // Initialize maximum element
+            int max = arr[0];
+
+            // Traverse array elements from second and
+            // compare every element with current max
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i] > max)
+                {
+                    max = arr[i];
+                }
+            }
+
+            Console.WriteLine($"Largest in given "
+                              + "array is " + max);
+        }
+    }
+}
+
+
